77cf6dd99e07aa53516954f593b0b23ac2ac9559
Ante Laca <ante.laca@get10up.com>
2022-08-23 12:03:44 +0200

    refactor: update

--- bin/vrt-tool.js
+++ bin/vrt-tool.js
@@ -10,3 +10,5 @@ program.command('init', 'Initialize VRT', {executableFile: 'init'});
 program.command('setup', 'Setup VRT', {executableFile: 'setup'});
 program.command('reports', 'List VRT reports', {executableFile: 'reports'});
 program.command('test', 'Run VRT', {executableFile: 'test'});
+
+program.parse();
